ab2f41cf54f95b524a3fb6a1163979971ed929a0
hejmi <michael.hejl@icloud.com>
2025-04-27 10:04:26 +0200

    mobile fix

--- Frontend/credit-card-validation/src/App.tsx
+++ Frontend/credit-card-validation/src/App.tsx
@@ -4,9 +4,9 @@ import MaxSum from './components/MaxSum'
 
 function App() {
 	return (
-		<div className="flex-col flex items-center justify-center">
+		<div className="flex-col flex items-center justify-center ">
 			<CreditCardForm />
-			<div className="mt-10 mb-10 flex flex-row gap-5 justify-start items-start">
+			<div className="mt-10 mb-10 flex sm:flex-row flex-col gap-5 justify-start sm:items-start items-center">
 				<InputCleaner />
 				<MaxSum />
 			</div>
